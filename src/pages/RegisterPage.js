381c681fb8924b9cf4e190342eb6b6220e31f7d1
pseudoureka <messiah7179@gmail.com>
2024-11-20 10:28:03 +0900

    RegisterPage 적용

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -9,6 +9,7 @@ import Link from "../components/Link";
 import GoogleImage from "../assets/google.svg";
 import styles from "./RegisterPage.module.css";
 import { useToaster } from "../contexts/ToasterProvider";
+import { useAuth } from "../contexts/AuthProvider";
 
 function RegisterPage() {
   const [values, setValues] = useState({
@@ -19,6 +20,7 @@ function RegisterPage() {
   });
   const navigate = useNavigate();
   const toast = useToaster();
+  const { login } = useAuth();
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -49,10 +51,7 @@ function RegisterPage() {
       password,
     });
 
-    await axios.post("/auth/login", {
-      email,
-      password,
-    });
+    await login({ email, password });
     navigate("/me");
   }
 
