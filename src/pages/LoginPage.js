034892f8097c16a2ad612548c22ab110b66aa6da
pseudoureka <messiah7179@gmail.com>
2024-11-20 10:23:22 +0900

    마이페이지, app context 작성

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "../lib/axios";
 import Label from "../components/Label";
 import Input from "../components/Input";
 import Button from "../components/Button";
