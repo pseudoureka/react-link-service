2f7be4ad46d5b4204959a89cd571f475999a761e
pseudoureka <messiah7179@gmail.com>
2024-11-25 13:05:47 +0900

    HomePage, LoginPage, RegisterPage 에서 user 값이 있을 때 MyPage 로 리다이렉트 시키기

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,27 @@
-import Button from '../components/Button';
-import Link from '../components/Link';
-import HeroImage from '../assets/hero.png';
-import HeroPlaceholderImage from '../assets/hero-placeholder.png';
-import styles from './HomePage.module.css';
+import Button from "../components/Button";
+import Link from "../components/Link";
+import HeroImage from "../assets/hero.png";
+import HeroPlaceholderImage from "../assets/hero-placeholder.png";
+import styles from "./HomePage.module.css";
+import { useAuth } from "../contexts/AuthProvider";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function HomePage() {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/me");
+    }
+  }, [user, navigate]);
+
   return (
     <div>
       <header className={styles.Header}>
         <h1 className={styles.Heading}>
-          나의 링크들을 <span className={styles.accent}>하나로</span>{' '}
-          관리하세요.
+          나의 링크들을 <span className={styles.accent}>하나로</span> 관리하세요.
         </h1>
         <p className={styles.Description}>
           파편화된 모든 정보들을, 이제는 하나의 프로필로 관리해봐요.
